83a3504fcba08b29e2a01acd8eb11f13ed81dd81
Fajar Ulin Nuha <fajarunuha@gmail.com>
2017-10-28 20:38:20 +0700

    edit config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   siteTitle: "Kumlaude", // Site title.
   siteTitleAlt: "Cumlaude Ilmu Komputer", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://kumlaude.surge.sh", // Domain of your website without pathPrefix.
+  siteUrl: "https://fajarnuha.github.io", // Domain of your website without pathPrefix.
   pathPrefix: "/kumlaude", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   siteDescription: "Your guide to become a great computer scientist", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
@@ -13,7 +13,7 @@ module.exports = {
   postDefaultCategoryID: "Tech", // Default category for posts.
   userName: "Kumlaude User", // Username to display in the author segment.
   userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
+  userLocation: "South Pole, Earth", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription: "Website ini memberikan segala informasi yang harapannya akan membantumu untuk menjadi cumlaude ilmu komputer. Informasi yang ada di website ini adalah kumpulan kontribusi mahasiswa-mahasiswa ilmu komputer yang di evaluasi oleh orang-orang terpercaya. Secara umum website ini memberikan informasi berdasarkan mata kuliah yang ada di ilkomp, di dalamnya, terdapat catatan, uts, uas, dan mungkin tips-tips terselubung!. Selamat belajar!", 
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
@@ -21,7 +21,7 @@ module.exports = {
   userLinks: [
     {
       label: "GitHub",
-      url: "https://github.com/Vagr9K/gatsby-material-starter",
+      url: "https://github.com/fajarnuha/kumlaude",
       iconClassName: "fa fa-github"
     },
     {
